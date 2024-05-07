e9690d658627ec26c160324a96d0b301121a62f2
sq1d0224 <isho05010224@gmail.com>
2024-05-07 10:56:00 +0000

    [Add]一覧表示の追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,13 +2,25 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/'
+  end
 
   def index
+    @books = Book.all
   end
 
   def show
   end
 
   def edit
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
