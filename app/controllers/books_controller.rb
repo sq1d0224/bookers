491ba78b483418fa1175deb2bbff022045aa4904
sq1d0224 <isho05010224@gmail.com>
2024-05-08 12:19:40 +0000

    [Add]一覧画面にshow,editリンク追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,7 +5,7 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to '/'
+    redirect_to book_path(book.id)
   end
 
   def index
